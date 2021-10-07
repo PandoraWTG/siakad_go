4bc8cc21daa799be0232b04d40961a6bdf7c173b
Taka <daffa.hp.dh@gmail.com>
2021-10-08 01:20:47 +0700

    Initialized go modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PandoraWTG/siakad_go
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+  "fmt"
+  user "github.com/PandoraWTG/siakad_go/user"
+)
+
+func main(){
+  fmt.Println("this is a test")
+  user.Test()
+}
